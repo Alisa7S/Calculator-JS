8588be8a495062c5073c9293a349526c1671dae3
Larisa Solovyova <larasolovyova76@yandex.ru>
2024-03-13 22:38:08 +0300

    Макет калькулятора

---
+++ script.js
@@ -0,0 +1,55 @@
+
+const a ="123"
+const bool = true
+const student = {
+     name:"Larisa",
+     surname:"Solovyova",
+     lastname:"Anatolevna",
+     age:"38",
+     Married:"true",
+     children:{
+        name:"Oleg",
+        surname:"Solovyov",
+        lastname:"Olegovich",
+        age:"18",
+        Married:"false",
+     },
+     sayName:()=>{
+        console.log(this.name)
+     }
+}
+
+const result ='6' / '2'
+//console.log('-----------')
+//console.log(typeof String(bool) )
+
+
+// 5 * 2
+// 5++ (5+1)
+// ++5 (1+5)
+//let x = -5
+//x = -x
+//x = x - x
+//console.log(x)
+
+//сложение +
+//вычитание -
+//множение  *
+//деление /
+//взятие остатка от деления % console.log( 5 % 2)
+//возведение в степень ** + 
+
+console.log("" + 1 + 0 )
+console.log("" - 1 + 0)
+console.log(true + false )
+console.log(6 / "3")
+console.log("2" * "3" )
+console.log(4 + 5 + "px")
+console.log("$" + 4 + 5)
+console.log("4" - 2)
+console.log("4px" - 2)
+console.log("-9" + 5)
+console.log("-9" - 5)
+console.log(null + 1)
+console.log(undefined + 1)
+console.log(" \t \n" - 2)
